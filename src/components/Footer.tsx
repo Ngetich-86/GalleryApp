d3779c2acb8c7de8f65e271432a13c463b2c257e
Ngetich-86 <gideonkngetich86@gmail.com>
2024-07-07 22:39:48 +0300

    all changes done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center bg-base-300 text-base-content p-4 mb-0">
+    <footer className="footer footer-center bg-success text-base-content p-4 mb-0">
   <aside>
     <p>
       © {new Date().getFullYear()} - All rights reserved by 
